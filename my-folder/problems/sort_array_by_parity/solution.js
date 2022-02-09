31c206e95e25fb79f0c7413209d0cd625579e22e
Teddy A <an410mu@gmail.com>
2022-02-09 07:30:25 +0000

    Sync LeetCode submission - Sort Array By Parity (javascript)

---
+++ my-folder/problems/sort_array_by_parity/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    var even = [];
+    var odd = [];
+    var output = [];
+    for(var i=0; i<nums.length; i++){
+        if(nums[i] %2 ==0){
+            even.push(nums[i]);
+        }else{
+            odd.push(nums[i])
+        }
+    }
+        output = even.concat(odd);
+    return output;
+    
+};
