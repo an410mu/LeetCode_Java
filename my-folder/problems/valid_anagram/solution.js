0a633e8ec2fdcf4e968829028780c8ea419b5a1d
Teddy A <an410mu@gmail.com>
2022-06-01 21:24:04 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- my-folder/problems/valid_anagram/solution.js
+++ my-folder/problems/valid_anagram/solution.js
@@ -4,15 +4,30 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    
-    if(sortter(s) === sortter(t)){
-        return true;
-    }else {
-        return false;
-    }
-    
-};
+    let obj1 = {};
+    let obj2 = {};
+    if (s.length !== t.length) return false;
+    s.split('').forEach(function(ele) {
+        if (obj1[ele] === undefined) {
+            obj1[ele] = 1;
+        } else {
+            obj1[ele] += 1;
+        }
+    });
 
-function sortter(str){
-    return str.split("").sort().join("");
-}
+    t.split('').forEach(function(ele) {
+        if (obj2[ele] === undefined) {
+            obj2[ele] = 1;
+        } else {
+            obj2[ele] += 1;
+        }
+    });
+    //console.log([obj1,obj2]);
+    for (let key in obj1) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+};
