3be2dbeb5c8a040ecbdb95ef6e2ad49e9c36a9f8
Teddy A <an410mu@gmail.com>
2022-08-27 23:35:05 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- my-folder/problems/two_sum/solution.js
+++ my-folder/problems/two_sum/solution.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let obj = {};
-    for(let i=0; i<nums.length; i++) {
+    let obj = {}
+    for (let i = 0; i < nums.length; i++) {
         obj[nums[i]] = i;
     }
-    for(let i=0; i<nums.length; i++) {
-        let complement = target - nums[i];
-        if(obj[complement] !== undefined && obj[complement] !== i){
-            return [i, obj[complement]];
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+        if (obj[complement] !== undefined && obj[complement] !== i) {
+            return [i, obj[complement]]
         }
     }
 };
