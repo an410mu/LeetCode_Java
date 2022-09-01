40b8954dec149db20b95e671124d4028d6f455e4
Teddy A <an410mu@gmail.com>
2022-09-01 05:34:43 +0000

    Sync LeetCode submission - Valid Palindrome (javascript)

---
+++ my-folder/problems/valid_palindrome/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let letter = /[^A-Za-z0-9]/g
+    s = s.toLowerCase().replace(letter, '')
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== s[s.length-1-i]) {
+            return false
+        }
+    }
+    return true; 
+};
