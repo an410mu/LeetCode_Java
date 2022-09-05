b306a02a2fe0f9cce052763cd12ab51d96aaa777
Teddy A <an410mu@gmail.com>
2022-09-05 06:27:07 +0000

    Sync LeetCode submission - Binary Search (javascript)

--- my-folder/problems/binary_search/solution.js
+++ my-folder/problems/binary_search/solution.js
@@ -4,17 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    var left = 0;
-    var right = nums.length - 1;
-
-    while(left <= right){
-        var mid = Math.floor((right + left)/2);
-        if(nums[mid] === target){
-            return mid;
-        }else if(target > nums[mid]){
-            left = mid + 1;
-        }else{
-            right = mid -1;
+    let l = 0;
+    let r = nums.length - 1;
+    while (l <= r) {
+        let mid = Math.floor((l + r)/2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (target > nums[mid]) {
+            l = mid + 1
+        } else if (target < nums[mid]) {
+            r = mid - 1
         }
     }
     return -1;
