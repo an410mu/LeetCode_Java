f10b256446072230794f260f34e8beb1d83a6c51
Teddy A <an410mu@gmail.com>
2022-09-15 22:09:00 +0000

    Sync LeetCode submission - Flood Fill (javascript)

--- my-folder/problems/flood_fill/solution.js
+++ my-folder/problems/flood_fill/solution.js
@@ -6,19 +6,22 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
-    if (image[sr][sc] === color) return image
+    if (image[sr][sc] === color) {
+        return image
+    }
     change(image, sr, sc, image[sr][sc], color)
-    return image;
+    return image
 }
 
-const change = function(image, sr, sc, old, color){
-    if (sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!==old) {
-        return 
+const change = function(image, sr, sc, old, color) {
+    if (sr < 0 || sc <0 || sr >= image.length || sc >= image[0].length || image[sr][sc] !== old) {
+        return
     }
     image[sr][sc] = color
-    change(image,sr+1, sc,old, color)
-    change(image,sr-1,sc,old,color)
-    change(image,sr, sc+1, old, color)
-    change(image,sr,sc-1,old, color)
-    return image;
+    change(image, sr+1, sc, old, color)
+    change(image, sr-1, sc, old, color)
+    change(image, sr, sc+1, old, color)
+    change(image, sr, sc-1, old, color)
+    return image
 }
+
