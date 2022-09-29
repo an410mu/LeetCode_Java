a214a35225f8d6288aacc817cce9d853550f7a0e
Teddy A <an410mu@gmail.com>
2022-09-29 20:29:13 +0000

    Sync LeetCode submission - Reverse Linked List (javascript)

--- my-folder/problems/reverse_linked_list/solution.js
+++ my-folder/problems/reverse_linked_list/solution.js
@@ -10,19 +10,22 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let output = new ListNode()
+    if (!head) {
+        return null
+    }
     let stack = []
     let cur = head
     while(cur) {
         stack.push(cur.val)
         cur = cur.next
     }
-    console.log(stack)
-    let tail = output;
+    
+    let output = new ListNode();
+    let tail = output
     while(stack.length > 0) {
         let node = new ListNode(stack.pop())
         tail.next = node
         tail = tail.next
     }
-    return output.next 
+    return output.next
 };
