eda0fda13be4badee0c975c0db7330d12e1b84d2
Teddy A <an410mu@gmail.com>
2022-09-30 05:29:46 +0000

    Sync LeetCode submission - Asteroid Collision (javascript)

---
+++ my-folder/problems/asteroid_collision/solution.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    
+    //iterate the array, push all positive
+    //if negative, check condition
+    //if less than top of stack, do nothing 
+    //if equal to top of stack, pop
+    //if greater than top, loop till less than top of stack 
+    let stack = []
+    for (let i = 0; i < asteroids.length; i++) {
+        if (stack.length === 0 || asteroids[i] > 0) {
+            stack.push(asteroids[i])
+        } else {
+            while (true) {
+                if (stack[stack.length -1] < 0) {
+                    stack.push(asteroids[i])
+                    break;
+                } else if (-asteroids[i] < stack[stack.length - 1]) {
+                    break;
+                } else if (-asteroids[i] === stack[stack.length -1]) {
+                    stack.pop()
+                    break;
+                } else {
+                    stack.pop()
+                    if (stack.length === 0) {
+                        stack.push(asteroids[i])
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return stack
+};
+
+
