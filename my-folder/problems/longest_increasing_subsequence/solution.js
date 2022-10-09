4d7cd41c47e9802f993d20ee93a61f679a79475c
Teddy A <an410mu@gmail.com>
2022-10-09 22:09:28 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (javascript)

---
+++ my-folder/problems/longest_increasing_subsequence/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let longest = 0
+    let arr = new Array(nums.length).fill(1)
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                arr[i] = Math.max(arr[i], arr[j] + 1)
+            }
+        }
+    }
+    console.log(arr)
+    return Math.max(...arr)
+};
+
