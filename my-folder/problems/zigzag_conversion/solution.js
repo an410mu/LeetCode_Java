d53899bef14d8336deff2f7f171faf6eb3adb4a5
Teddy A <an410mu@gmail.com>
2022-10-15 06:51:14 +0000

    Sync LeetCode submission - Zigzag Conversion (javascript)

---
+++ my-folder/problems/zigzag_conversion/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1) return s
+    let output =''
+    for (let r = 0; r < numRows; r++) {
+        let increment = 2 * (numRows - 1)
+        for (let i = r; i < s.length; i+=increment) {
+            output += s[i]
+            if (r > 0 && r < numRows - 1 && i + increment - 2 * r < s.length) {
+                output += s[i + increment - 2*r]
+            }
+        }
+    }
+    return output
+};
+
+
