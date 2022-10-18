4aaf445784fdc9119bbb41d1bd4616bf4fb1a8d5
Teddy A <an410mu@gmail.com>
2022-10-18 18:56:48 +0000

    Sync LeetCode submission - 3Sum (javascript)

--- my-folder/problems/3sum/solution.js
+++ my-folder/problems/3sum/solution.js
@@ -5,40 +5,38 @@
 var threeSum = function(nums) {
     let output = []
     nums.sort((a,b) => a-b)
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) {
+    for(let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i-1]) {
             continue
         } else {
-            findCombo(nums, nums[i], i + 1, output)
+            //find 
+            findSum(nums, nums[i], i + 1, output)
         }
     }
     return output
 }
 
-const findCombo = (nums, target, left, output) => {
+const findSum = (nums, target, left, output) => {
     let right = nums.length - 1
-    while( left < right) {
+    while(left < right) {
         let curSum = nums[left] + nums[right]
         if (curSum === -target) {
             output.push([target, nums[left], nums[right]])
             left++
             right--
             while(left > 0 && nums[left] === nums[left - 1]) {
-              left++
+                left++
             }
-        
-            while(right > 0 && nums[right] === nums[right + 1]) {
-              right--
+            while (right>0 && nums[right] === nums[right + 1]) {
+                right--
             }
-        } else if (curSum > -target) {
+        }else if (curSum > -target) {
             right--
         } else {
-            left++
+            left ++
         }
-        
     }
 }
-
 
 
 
