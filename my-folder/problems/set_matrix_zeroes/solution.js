864bbca2c08c599032b0b8ae00c89941937edcf2
Teddy A <an410mu@gmail.com>
2022-11-13 19:49:02 +0000

    Sync LeetCode submission - Set Matrix Zeroes (javascript)

--- my-folder/problems/set_matrix_zeroes/solution.js
+++ my-folder/problems/set_matrix_zeroes/solution.js
@@ -3,26 +3,24 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let zeroArray = [];
-    for(let i = 0; i < matrix.length; i++) {
-        for(let j = 0; j < matrix[0].length; j++) {
-            if (matrix[i][j] === 0) {
-                zeroArray.push([i, j]);
-            }
+    let index = []
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === 0) index.push([i, j])
         }
     }
-    for (let index of zeroArray) {
-        let row, col;
-        [row, col] = index;
-        makeZero(row, col, matrix);
+    
+    for (let item of index) {
+        let [r, c] = item 
+        change (r, c, matrix)
     }
 };
 
-const makeZero = function(r, c, matrix) {
-    for(let i = 0; i < matrix.length; i++) {
-        matrix[i][c] = 0;
+const change = (r, c, matrix) => {
+    for (let i = 0; i < matrix[0].length; i++) {
+        matrix[r][i] = 0
     }
-    for(let i = 0; i < matrix[0].length; i++) {
-        matrix[r][i] = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i][c] = 0
     }
 }
