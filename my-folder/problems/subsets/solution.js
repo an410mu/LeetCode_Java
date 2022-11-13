09f6fb8b0841f026478fc761b5a49566f968fd83
Teddy A <an410mu@gmail.com>
2022-11-13 21:07:43 +0000

    Sync LeetCode submission - Subsets (javascript)

--- my-folder/problems/subsets/solution.js
+++ my-folder/problems/subsets/solution.js
@@ -3,18 +3,18 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    
+    nums.sort((a,b) => a -b)
     let output = []
-    find (nums, 0, [], output)
+    find(nums, 0, [], output)
     return output
 };
 
 const find = (nums, index, cur, output) => {
     output.push(Array.from(cur))
     for (let i = index; i < nums.length; i++) {
-        if (i === index || nums[i] !== nums[i - 1]) {
+        if (nums[i] !== nums[i - 1]) {
             cur.push(nums[i])
-            find(nums, i + 1, cur, output)
+            find(nums, i+1, cur, output)
             cur.pop()
         }
     }
