652c11a05e8705f817ac4b1c83892108b30916c0
Teddy A <an410mu@gmail.com>
2022-11-27 04:02:20 +0000

    Sync LeetCode submission - Coin Change (javascript)

--- my-folder/problems/coin_change/solution.js
+++ my-folder/problems/coin_change/solution.js
@@ -6,16 +6,18 @@
 var coinChange = function(coins, amount) {
     let dp = []
     const find = (coins, amount) => {
-        if (amount < 0 ) return Infinity
-        if (amount === 0) return 0
-        if (dp[amount] !== undefined) return dp[amount]
         let min = Infinity
+        if (amount === 0 ) return 0
+        if (amount < 0) return Infinity
+        if (dp[amount] !== undefined) {
+            return dp[amount]
+        }
+        
         for (let coin of coins) {
             min = Math.min(min, find(coins, amount - coin) + 1)
         }
         dp[amount] = min
         return dp[amount]
-        
     }
     let res = find(coins, amount)
     return res === Infinity ? -1 : res
