f5f99bd4db5328d9486bbcb1f3a58fb47fdf022c
Teddy A <an410mu@gmail.com>
2022-12-01 19:12:55 +0000

    Sync LeetCode submission - 3Sum (javascript)

--- my-folder/problems/3sum/solution.js
+++ my-folder/problems/3sum/solution.js
@@ -4,39 +4,37 @@
  */
 var threeSum = function(nums) {
     let output = []
-    nums.sort((a,b) => a-b)
-    for(let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i-1]) {
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
             continue
         } else {
-            //find 
-            findSum(nums, nums[i], i + 1, output)
+            find(nums, nums[i], output, i + 1)
         }
     }
     return output
-}
+};
 
-const findSum = (nums, target, left, output) => {
+const find = (nums, target, output, left) => {
     let right = nums.length - 1
     while(left < right) {
-        let curSum = nums[left] + nums[right]
-        if (curSum === -target) {
+        let curSum =  nums[left] + nums[right]
+        if (curSum + target === 0) {
             output.push([target, nums[left], nums[right]])
             left++
             right--
+        
             while(left > 0 && nums[left] === nums[left - 1]) {
                 left++
             }
-            while (right>0 && nums[right] === nums[right + 1]) {
+            while(right > 0 && nums[right] === nums[right + 1]) {
                 right--
             }
-        }else if (curSum > -target) {
-            right--
+        } else if(curSum < -target) {
+            left++
         } else {
-            left ++
+            right--
         }
+        
     }
 }
-
-
-
