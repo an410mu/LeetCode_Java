6130d8d927ca022c64a83a7e28674bfa9ba41fcf
Teddy A <an410mu@gmail.com>
2022-12-07 07:40:16 +0000

    Sync LeetCode submission - Valid Palindrome (javascript)

--- my-folder/problems/valid_palindrome/solution.js
+++ my-folder/problems/valid_palindrome/solution.js
@@ -5,6 +5,7 @@
 var isPalindrome = function(s) {
     let letter = /[^A-Za-z0-9]/g
     s = s.toLowerCase().replace(letter, '')
+        console.log(s)
     for (let i = 0; i < s.length; i++) {
         if (s[i] !== s[s.length-1-i]) {
             return false
