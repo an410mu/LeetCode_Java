6913b68b00e992c56878db480abd671037cca453
Teddy A <an410mu@gmail.com>
2022-12-16 04:27:37 +0000

    Sync LeetCode submission - Two Sum (python)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -5,11 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        dic = {}
-        for i in range(0,len(nums)):
-            dic[nums[i]]= i
-        for i in range(0,len(nums)):
-            complement = target - nums[i]
-            if complement in dic and dic[complement]!=i:
-                return [i,dic[complement]]
-        
+
+        obj = {}
+        for i in range(len(nums)):
+            if (target - nums[i] in obj):
+                return [obj[target - nums[i]], i]
+            else:
+                obj[nums[i]] = i
