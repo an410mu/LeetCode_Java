36a603b5cad1cf5b384adae192f2cae757e8f0c5
Teddy A <an410mu@gmail.com>
2023-02-02 04:52:12 +0000

    Sync LeetCode submission - Jump Game II (java)

---
+++ my-folder/problems/jump_game_ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int count = 0;
+        int left = 0;
+        int right = 0;
+        while (right < nums.length - 1) {
+            int maxJump = 0;
+            for (int i = left; i < right + 1; i++) {
+                maxJump = Math.max(maxJump, i + nums[i]);
+            }
+            left = right + 1;
+            right = maxJump;
+            count++;
+        }
+        return count;
+    }
+}
