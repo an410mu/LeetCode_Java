b6c9d988887293339268744d9facb8f5ed3ed56a
Teddy A <an410mu@gmail.com>
2023-02-21 19:56:59 +0000

    Sync LeetCode submission - Even Odd Tree (java)

---
+++ LeetCode_Java/problems/even_odd_tree/solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+
+       queue.add(root);
+       int level = 0;
+       int prev = 0;
+       while(!queue.isEmpty()) {
+           int size = queue.size();
+           for (int i = 0; i < size; i++) {
+               TreeNode cur = queue.poll();
+               //even
+                if (level % 2 == 0 && (cur.val % 2 == 0 || (i > 0 && cur.val <= prev))) {
+                    return false;
+                }
+                if (level % 2 != 0 && (cur.val % 2 !=0 || (i > 0 && cur.val >= prev))) {
+                    return false;
+                }
+
+               prev = cur.val;
+               if (cur.left != null) {
+                   queue.add(cur.left);
+               }
+               if (cur.right != null) {
+                   queue.add(cur.right);
+               }
+           }
+           level++;
+       }
+       return true;
+    }
+}
