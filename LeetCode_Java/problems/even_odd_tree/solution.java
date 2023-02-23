935f3be7793d8fee012b5730dbdad4fadefa677c
Teddy A <an410mu@gmail.com>
2023-02-23 23:27:06 +0000

    Sync LeetCode submission - Even Odd Tree (java)

--- LeetCode_Java/problems/even_odd_tree/solution.java
+++ LeetCode_Java/problems/even_odd_tree/solution.java
@@ -16,32 +16,33 @@
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
-
-       queue.add(root);
-       int level = 0;
-       int prev = 0;
-       while(!queue.isEmpty()) {
-           int size = queue.size();
-           for (int i = 0; i < size; i++) {
-               TreeNode cur = queue.poll();
-               //even
+        int level = 0;
+        int prev = 0;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                //check odd, even
                 if (level % 2 == 0 && (cur.val % 2 == 0 || (i > 0 && cur.val <= prev))) {
                     return false;
                 }
-                if (level % 2 != 0 && (cur.val % 2 !=0 || (i > 0 && cur.val >= prev))) {
+                if (level % 2 != 0 && (cur.val % 2 != 0 || (i > 0 && cur.val >= prev))) {
                     return false;
                 }
 
-               prev = cur.val;
-               if (cur.left != null) {
-                   queue.add(cur.left);
-               }
-               if (cur.right != null) {
-                   queue.add(cur.right);
-               }
-           }
-           level++;
-       }
-       return true;
+                //add child back to queue
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+                prev = cur.val;
+            }
+            level++;
+        }
+        return true;
     }
+
 }
